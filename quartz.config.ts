de3bda48a097530663ff65daad28e729b19ef4b7
vsegaux <segauxvirgile@gmail.com>
2025-09-23 18:35:26 +0200

    Quartz sync: Sep 23, 2025, 6:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "fr-FR",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
